faa217b098e6879c1722f543ca53ac2c9d9c4c22
Riccardo Gucciardo <riccardogucciardo003@gmail.com>
2025-01-22 17:37:56 +0100

    fine esercizio

--- js-simon/script.js
+++ js-simon/script.js
@@ -2,37 +2,47 @@
 //Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i
 //numeri che ha visto precedentemente, nell'ordine che preferisce.
  
-//let timer = setInterval(myfunction, 7500)
-//const listNumber = document.getElementById("numbers-list")
-let casualN1 = Math.floor(Math.random()* 50)+ 1 
-let casualN2 = Math.floor(Math.random()* 50)+ 1 
-let casualN3 = Math.floor(Math.random()* 50)+ 1 
-let casualN4 = Math.floor(Math.random()* 50)+ 1 
-let casualN5 = Math.floor(Math.random()* 50)+ 1 
 
-document.getElementById("numbers-list1").innerHTML = casualN1
-document.getElementById("numbers-list2").innerHTML = casualN2
-document.getElementById("numbers-list3").innerHTML = casualN3
-document.getElementById("numbers-list4").innerHTML = casualN4
-document.getElementById("numbers-list5").innerHTML = casualN5
 
-const numberList =document.getElementById("input-group")
-const numArrey = []
+
+const numberList =document.getElementById("number-list")
+const numArray = []
+
+function numeriRandom(min,max){
+   return Math.floor(Math.random()* max)+ min
+}
+   
+for(let i= 0;numArray.length < 5; i++){
+
+    let numrR = numeriRandom (1 , 50)     
+    
+
+    console.log(`il tuo numero random è :${numrR}`);
+    
+
+    if(!(numArray.includes(numrR) ) ) {
+        numArray.push(numrR)
+    }
+
+console.log(numArray);
+
+}
 let contatore = 3
 const countdown = document.getElementById("countdown")
+let scomparsaIstruzioni = document.getElementById('instructions')
+let answersForm = document.getElementById("answers-form")
 
 
-    let scomparsaNumeri = document.getElementById('scomparsa')
-    let scomparsaIstruzioni = document.getElementById('instructions')
-    let answersForm = document.getElementById("answers-form")
-    
+for(let i= 0; i< numArray.length; i++){
+    numberList.innerHTML += `<li> ${ numArray[i] } </li>`
+}
     const timer = setInterval(function(){
     countdown.innerHTML = contatore--
 
 
     if( contatore < 0 ) {
     clearInterval( timer )
-    scomparsaNumeri.classList.add('hidden')
+    numberList.classList.add('hidden')
     scomparsaIstruzioni.classList.add('hidden')
     answersForm.classList.remove("d-none") 
 }
@@ -41,7 +51,40 @@ const countdown = document.getElementById("countdown")
 
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri
 //da indovinare sono stati individuati.
+const bottone = document.querySelector(".btn-primary")
+const input = document.querySelectorAll(".form-control")
+const numeriSceltiC = []
+const mesaggio =document.getElementById("message")
 
+
+    
+
+
+
+
+
+
+
+
+console.log(input);
+
+bottone.addEventListener('click', function(event){
+    event.preventDefault()
+
+    for( let i = 0; i < input.length; i++ ){
+        
+        if(numArray.includes( parseInt(input[i].value )) ){
+
+            numeriSceltiC.push(input[i].value)
+
+        }
+
+        console.log(numeriSceltiC, input[i].value,numArray.includes(parseInt(input[i].value)));
+        
+    }
+
+mesaggio.innerHTML =`complimenti hai indovinato${numeriSceltiC.length}, e sono ${numeriSceltiC.join("-")}`
+})
 
 
 
